57eff7249cbcb26720744c9b485191371b1a950b
Zen Sirzechs <39871161+LDKLucifer@users.noreply.github.com>
2020-02-04 21:59:16 +0600

    bEtTeR (#5)

--- src/main/java/me/petterim1/scoreboards/ScoreboardUpdater.java
+++ src/main/java/me/petterim1/scoreboards/ScoreboardUpdater.java
@@ -2,8 +2,6 @@ package me.petterim1.scoreboards;
 
 import cn.nukkit.Player;
 
-import com.creeperface.nukkit.placeholderapi.api.PlaceholderAPI;
-
 import de.theamychan.scoreboard.api.ScoreboardAPI;
 import de.theamychan.scoreboard.network.DisplaySlot;
 import de.theamychan.scoreboard.network.Scoreboard;
@@ -23,22 +21,25 @@ public class ScoreboardUpdater extends Thread {
     @Override
     public void run() {
         try {
-            for (Player p : plugin.getServer().getOnlinePlayers().values()) {
-                try {
-                    Main.scoreboards.get(p).hideFor(p);
-                } catch (Exception ignored) {}
+            if (!plugin.getServer().getOnlinePlayers().isEmpty()) {
+                for (Player p : plugin.getServer().getOnlinePlayers().values()) {
 
-                Scoreboard scoreboard = ScoreboardAPI.createScoreboard();
-                ScoreboardDisplay scoreboardDisplay = scoreboard.addDisplay(DisplaySlot.SIDEBAR, "dumy", Main.config.getString("title"));
+                    Scoreboard scoreboard = ScoreboardAPI.createScoreboard();
+                    ScoreboardDisplay scoreboardDisplay = scoreboard.addDisplay(DisplaySlot.SIDEBAR, "dumy", Main.config.getString("title"));
 
-                Main.config.getStringList("text").forEach((text) -> {
-                    scoreboardDisplay.addLine(Main.getScoreboardString(p, text), line++);
-                });
+                    Main.config.getStringList("text").forEach((text) -> scoreboardDisplay.addLine(Main.getScoreboardString(p, text), line++));
 
-                scoreboard.showFor(p);
-                Main.scoreboards.put(p, scoreboard);
-                line = 0;
+                    try {
+                        Main.scoreboards.get(p).hideFor(p);
+                    } catch (Exception ignored) {
+                    }
+
+                    scoreboard.showFor(p);
+                    Main.scoreboards.put(p, scoreboard);
+                    line = 0;
+                }
             }
-        } catch (Exception ignored) {}
+        } catch (Exception ignored) {
+        }
     }
 }
